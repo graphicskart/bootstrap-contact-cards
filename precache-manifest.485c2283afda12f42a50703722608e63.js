443f1aa3babe8dadd6fa7b112a1bf719f04e1549
graphicskart <graphicskart2018@gmail.com>
2019-03-31 22:01:28 +0530

    Updates

---
+++ precache-manifest.485c2283afda12f42a50703722608e63.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d87f31249a699e65361689139951ef3",
+    "url": "/bootstrap-contact-cards/static/media/user4.4d87f312.jpg"
+  },
+  {
+    "revision": "6f5a0c658fb2052d580fc2c5ffff6220",
+    "url": "/bootstrap-contact-cards/static/media/user3.6f5a0c65.jpg"
+  },
+  {
+    "revision": "9a5ab8d7ca597ba18115827c452245e3",
+    "url": "/bootstrap-contact-cards/static/media/user2.9a5ab8d7.jpg"
+  },
+  {
+    "revision": "da2e546841da40cdcf60061743233500",
+    "url": "/bootstrap-contact-cards/static/media/user1.da2e5468.jpg"
+  },
+  {
+    "revision": "b14e0cef97e1217ecca1",
+    "url": "/bootstrap-contact-cards/static/js/runtime~main.09e170b2.js"
+  },
+  {
+    "revision": "9d3533e30856677f5087",
+    "url": "/bootstrap-contact-cards/static/js/main.47759b36.chunk.js"
+  },
+  {
+    "revision": "1738b577c38e731358f9",
+    "url": "/bootstrap-contact-cards/static/js/2.d55d8ecb.chunk.js"
+  },
+  {
+    "revision": "9d3533e30856677f5087",
+    "url": "/bootstrap-contact-cards/static/css/main.a509bebe.chunk.css"
+  },
+  {
+    "revision": "126eed1f3d933371c87628bf642d3c80",
+    "url": "/bootstrap-contact-cards/index.html"
+  }
+];
